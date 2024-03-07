0a52063aa44b49f64a0a572d749dee5363b3e2c5
SzKriszti <krisztinaszabo98@gmail.com>
2024-03-07 11:55:29 +0100

    basic forms according title

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,18 @@
-function Form() {
+import { useState } from "react"
+
+function Form({ title }) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
-    <div>Form</div>
+    <form>
+      <h3>{title} form</h3>
+
+      <input type="text" autoComplete="off" placeholder="e-mail addres"></input>
+      <input type="password" autoComplete="off" placeholder="password"></input>
+
+      <button type="button">{title}</button>
+    </form>
   )
 }
 
